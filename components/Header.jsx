e07e351b4182afe0cc17a292671ef7d6ad18deb5
German Bilalov <pinncho@gmail.com>
2022-02-14 20:44:15 +0600

    feat(ui): add Header component

---
+++ components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Navbar from './Navbar';
+import styles from '../styles/Header.module.css';
+
+function Header({ queryMovies }) {
+    return (
+        <div className={styles.header}>
+            <Navbar queryMovies={queryMovies} />
+            <div className="container">
+                <div className={styles.descriptionBox}>
+                    <h1>John Wick 3 : Parabellum</h1>
+                    <div className={styles.ratings}>
+                        <span className={styles.ratingItem}>
+                            <img src="/images/imbd.png" alt="IMBD logo" />
+                            86.0 / 100
+                        </span>
+                        <span className={styles.ratingItem}>
+                            <img src="/images/tomato.png" alt="IMBD logo" />
+                            97%
+                        </span>
+                    </div>
+                    <p>
+                        John Wick is on the run after killing a member of the
+                        international assassins' guild, and with a $14 million
+                        price tag on his head, he is the target of hit men and
+                        women everywhere.
+                    </p>
+                    <button className={styles.button}>
+                        <img src="/svg/circle-play.svg" alt="Play" />
+                        Watch trailer
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
