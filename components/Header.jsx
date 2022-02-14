ec22f1b4b28de9d3dd29a95643e00982f2dcf853
German Bilalov <pinncho@gmail.com>
2022-02-14 20:56:32 +0600

    fix(ui): replace appos with special symbol

--- components/Header.jsx
+++ components/Header.jsx
@@ -21,9 +21,9 @@ function Header({ queryMovies }) {
                     </div>
                     <p>
                         John Wick is on the run after killing a member of the
-                        international assassins' guild, and with a $14 million
-                        price tag on his head, he is the target of hit men and
-                        women everywhere.
+                        international assassins&apos; guild, and with a $14
+                        million price tag on his head, he is the target of hit
+                        men and women everywhere.
                     </p>
                     <button className={styles.button}>
                         <img src="/svg/circle-play.svg" alt="Play" />
